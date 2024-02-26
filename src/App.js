fb9056ae4a671e38dc712aa9cb1f47cdc884d4df
Henry Paez <hpaezgutierrez@gmail.com>
2024-02-25 19:52:48 -0500

    add GenreSelect

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import { useState } from 'react';
 import './App.css';
 import Counter from './Counter.js';
 import SearchForm from './SearchForm.js';
+import GenreSelect from './GenreSelect.js';
 
 function App() {
 
   const [filterText, setFilterText] = useState('');
+  let displayList = [{name: 'Crime', id: 1},{name: 'Documentary', id: 2},
+    {name: 'Horror', id: 3}, {name: 'Comedy', id: 4}];
+  let selectedGenre = [{name: 'Crime', id: 1},{name: 'Horror', id: 3}];
 
   return (
     <div className="container">
@@ -14,7 +18,14 @@ function App() {
             <Counter initialValue={100} />
        </div>
        <div className="row">
-            <SearchForm filterText={filterText}  onSearch={(event) => { event.preventDefault();console.log(filterText);}} onFilterTextChange={setFilterText} />
+            <SearchForm filterText={filterText}  
+            onSearch={(event) => { event.preventDefault();console.log(filterText);}} 
+            onFilterTextChange={setFilterText} />
+       </div>
+       <div className="row">
+            <GenreSelect displayList={displayList}  
+            onSelect={(selectedItem) => { console.log(`Genre was added`) }} 
+            selectedGenre={selectedGenre} />
        </div>
     </div>
   );
